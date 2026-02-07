2b6f4b621c1b50efe5015fbcbc07e4e72b3b31f5
Raul SMCR <raul.olmedo@gmail.com>
2026-02-06 21:27:51 -0300

    lay out paciente

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 // src/app/layout.js
 import './globals.css';
-import Header from '@/components/Header'; // <--- SIN LAS BARRAS //
+import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 export const metadata = {
@@ -11,9 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="min-h-screen bg-[rgb(var(--app-bg))] text-neutral-900 antialiased">
-        <Header />  {/* <--- SIN LAS BARRAS // */}
-        <main className="container py-8">{children}</main>
+      {/* 1. flex flex-col: Permite organizar header-main-footer verticalmente.
+         2. min-h-screen: Asegura que el cuerpo ocupe al menos toda la altura de la ventana.
+      */}
+      <body className="min-h-screen flex flex-col bg-slate-50 text-neutral-900 antialiased">
+        
+        <Header />
+        
+        {/* flex-grow: Empuja el footer hacia abajo si el contenido es corto.
+           Quitamos 'container': Ahora cada página (page.js) decide sus márgenes.
+        */}
+        <main className="flex-grow">
+          {children}
+        </main>
+        
         <Footer />
       </body>
     </html>
